31b8a8c024f851991b9336f439f6e4fed76959cd
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-21 23:57:06 +0530

    format fixes

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,82 +1,87 @@
-async function getData(){
-    try{
-        const response = await fetch("http://localhost:8000/todos/get-todos");
-        const data = await response.json();
-        console.log(data);
-        displayData(data.todos);
-    }catch(err){
-        console.log(err);
-    }
+async function getData() {
+  try {
+    const response = await fetch("http://localhost:8000/todos/get-todos");
+    const data = await response.json();
+    console.log(data);
+    displayData(data.todos);
+  } catch (err) {
+    console.log(err);
+  }
 }
 window.onload = () => {
-    getData();
+  getData();
+};
+
+async function deleteTodo(id) {
+  try {
+    const response = await fetch(
+      `http://localhost:8000/todos/delete-todo/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const data = await response.text();
+    alert(data);
+    getData(); // refresh UI
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function deleteTodo(id){
-    try{
-        const response = await fetch(`http://localhost:8000/todos/delete-todo/${id}`,{
-            method: "DELETE"
-        });
-        const data = await response.text();
-        alert(data);
-        getData(); // refresh UI
-    }catch(err){
-        console.log(err);
-    }
+async function updateTodo(id) {
+  try {
+    const updatedTodo = {
+      status: true,
+    };
+    const response = await fetch(
+      `http://localhost:8000/todos/update-todo/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(updatedTodo),
+      }
+    );
+
+    const data = await response.text();
+    alert(data);
+    getData(); //refresh UI
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function updateTodo(id){
-    try{
-        const updatedTodo = {
-            status: true
-        }
-        const response = await fetch(`http://localhost:8000/todos/update-todo/${id}`,{
-            method: "PUT",
-            headers:{
-                "content-type":"application/json"
-            },
-            body: JSON.stringify(updatedTodo)
-        });
-
-        const data = await response.text();
-        alert(data);
-        getData(); //refresh UI
-    }catch(err){
-        console.log(err);
-    }
-}
-
-function displayData(arr){
-    let container = document.getElementById("container");
-    let todosArray = arr.map((el,i)=>{
-        let card = `<div class="card" data-id="${el.id}">
+function displayData(arr) {
+  let container = document.getElementById("container");
+  let todosArray = arr.map((el, i) => {
+    let card = `<div class="card" data-id="${el.id}">
             <h2>${el.name}</h2>
-            <h3>${el.status?"Completed":"Pending"}</h3>
+            <h3>${el.status ? "Completed" : "Pending"}</h3>
             <div class="btns">
                 <button class="update-btn">Update</button>
                 <button class="delete-btn">Delete</button>
             </div>
         </div>`;
-        return card;
+    return card;
+  });
+  container.innerHTML = todosArray.join(" ");
+
+  //adding listeners for delete and update buttons
+
+  document.querySelectorAll(".delete-btn").forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      const card = e.target.closest(".card");
+      const id = card.dataset.id;
+      await deleteTodo(id);
     });
-    container.innerHTML = todosArray.join(" ");
+  });
 
-    //adding listeners for delete and update buttons
-
-    document.querySelectorAll(".delete-btn").forEach((btn)=>{
-        btn.addEventListener("click", async (e)=>{
-            const card = e.target.closest(".card");
-            const id = card.dataset.id;
-            await deleteTodo(id);
-        })
-    })
-
-    document.querySelectorAll(".update-btn").forEach((btn)=>{
-        btn.addEventListener("click", async (e)=>{
-            const card = e.target.closest(".card");
-            const id = card.dataset.id;
-            await updateTodo(id);
-        })
-    })
-
+  document.querySelectorAll(".update-btn").forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      const card = e.target.closest(".card");
+      const id = card.dataset.id;
+      await updateTodo(id);
+    });
+  });
 }
