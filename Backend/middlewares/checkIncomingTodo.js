1b170a71feb58dec52dce65208794905a07269ea
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-23 23:54:56 +0530

    todo body checking middleware fix

--- Backend/middlewares/checkIncomingTodo.js
+++ Backend/middlewares/checkIncomingTodo.js
@@ -1,17 +1,15 @@
+const checkIncomingTodo = (req, res, next) => {
+  const allowedKeys = ["id", "name", "status"];
+  const keys = Object.keys(req.body);
 
+  const hasOnlyAllowedKeys = keys.every((key) => allowedKeys.includes(key));
+  const hasRequiredKeys = allowedKeys.every((key) => key in req.body);
 
-const checkIncomingTodo = (req,res,next) => {
-    const allowedKeys = ["id","name","status"];
-    const keys = Object.keys(req.body);
-
-    const hasOnlyAllowedKeys = keys.every(key => allowedKeys.includes(key));
-    const hasRequiredKeys = allowedKeys.every(key => key in req.body);
-
-    if(!hasRequiredKeys || !hasOnlyAllowedKeys){
-        res.send("invalid todo format");
-    }else{
-        next();
-    }
-}
+  if (!hasRequiredKeys || !hasOnlyAllowedKeys) {
+    res.send("invalid todo format");
+  } else {
+    next();
+  }
+};
 
 module.exports = checkIncomingTodo;
