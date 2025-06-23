075b7bd5cb1b9cfd576630b8c1a8fc6dd0778aa8
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-23 23:54:12 +0530

    logger middleware fix

--- Backend/middlewares/logger.js
+++ Backend/middlewares/logger.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 const loggerMiddleware = (req,res,next) => {
     // this middleware just logs all requests in a file
-    let data =`End-point: ${req.url} | Method: ${req.method} | Date-Time: ${Date.now()} \n`;
+    let data =`End-point: ${req.url} | Method: ${req.method} | Date-Time: ${new Date().toISOString()} \n`;
     fs.appendFileSync("./logs.txt",data);
     next();
 }
